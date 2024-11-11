378aa3eec122582f5533da82337b4858719ea1da
Shakeebraza <Muhammad.Shakeeb@fissionfox.com>
2024-11-12 04:47:31 +0500

    fix:"html simple to test"

--- test.php
+++ test.php
@@ -1,4 +1,25 @@
 <?php
-include_once('global.php');
-echo smtp_mailer('shakeebrazamuhammad@gmail.com', 'Subject', 'Body of the email');
+// include_once('global.php');
+// echo smtp_mailer('shakeebrazamuhammad@gmail.com', 'Subject', 'Body of the email');
 ?>
+
+<html>
+
+<head>
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3145087323601863"
+crossorigin="anonymous"></script>
+
+   This is the head of your page.
+
+   <title>Example HTML page</title>
+
+</head>
+
+<body>
+
+   This is the body of your page.
+
+</body>
+
+</html> 
